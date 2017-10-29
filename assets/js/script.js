cf3487565b50d14224b4bda9b6e2074f5d8f4c87
Morozov Aleksandr <bady_bon@mail.ru>
2017-10-29 01:05:11 +0000

    filling alt , title. Add symbols mnemonics.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+/*for stream*/
 var elStream = document.getElementsByClassName('stream');
 var elBlock = document.getElementsByClassName('stream__block');
 for (var i = 0; i < elStream.length; i++) {
@@ -14,6 +15,8 @@ function z(a) {
     }
 }
 
+
+/*for fixed menubar*/
 $(document).ready(function() {
     $(".menubar").sticky({
         topSpacing: 0,
@@ -21,3 +24,20 @@ $(document).ready(function() {
     });
 });
 
+
+
+/*for button scroll-top*/
+$(document).ready(function() {
+    $().UItoTop({ easingType: 'easeOutQuart' });
+
+});
+
+
+/*for tabs in media*/
+$(document).ready(function() {
+    $(".tab__item").not(":first").hide();
+    $(".wrapper .tab").click(function() {
+        $(".wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+        $(".tab__item").hide().eq($(this).index()).fadeIn()
+    }).eq(0).addClass("active");
+});
